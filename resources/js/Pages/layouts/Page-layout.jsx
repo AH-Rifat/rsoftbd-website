8178005c1c3f9e94e5dc8eb34d4f656ae4abf728
ah-rifat <rifat266@gmail.com>
2025-10-01 23:54:00 +0600

    feat: add session error+sucess message

--- resources/js/Pages/layouts/Page-layout.jsx
+++ resources/js/Pages/layouts/Page-layout.jsx
@@ -1,7 +1,19 @@
+import { usePage } from "@inertiajs/react";
 import Footer from "../components/Footer";
 import HeaderNav from "../components/HeaderNav";
+import { useEffect } from "react";
 
 export default function PageLayout({ children }) {
+    const { flash } = usePage().props;
+    useEffect(() => {
+        if (flash.success) {
+            alert(flash.success);
+        }
+        if (flash.error) {
+            alert(flash.error);
+        }
+    }, [flash.success, flash.error]);
+
     return (
         <>
             <HeaderNav />
