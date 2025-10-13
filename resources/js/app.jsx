17e440434e5b860bbef171dc4ed0b33355bb26b0
ah-rifat <rifat266@gmail.com>
2025-10-13 18:14:44 +0600

    add progress bar + website dynamic title

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,17 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-    resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
-        return pages[`./Pages/${name}.jsx`]
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
     },
     setup({ el, App, props }) {
-        createRoot(el).render(<App {...props} />)
+        createRoot(el).render(<App {...props} />);
     },
-})
+    progress: {
+        delay: 250,
+        color: "oklch(76.5% 0.177 163.223)",
+    },
+    title: (title) => `${title} - R-softBD`,
+});
