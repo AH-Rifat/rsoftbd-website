dc8b285a0421b6cfbb72db5e80965b8c0d10693d
ah-rifat <rifat266@gmail.com>
2025-10-28 10:43:58 +0600

    feat: seed complete gym software pricing plans

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductPricePlan;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,66 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        ProductPricePlan::insert([
+            [
+                'product' => 'gym-software',
+                'name' => 'Basic',
+                'price' => 19500.00,
+                'period' => 'One-Time',
+                'popular' => false,
+                'features' => json_encode([
+                    "Can manage 2,000 members",
+                    "Can accept payments offline",
+                    "Member attendance can be tracked",
+                    "SMS system integrated",
+                    "Membership package tracking",
+                    "2 users can use simultaneously",
+                    "24/7 customer support",
+                    "Basic financial reporting",
+                ]),
+            ],
+            [
+                'product' => 'gym-software',
+                'name' => 'Premium',
+                'price' => 29500.00,
+                'period' => 'One-Time',
+                'popular' => true,
+                'features' => json_encode([
+                    "Can manage 5,000 members",
+                    "Can accept payments online/offline",
+                    "Member attendance can be tracked",
+                    "SMS system integrated",
+                    "Membership package tracking",
+                    "5 users can use simultaneously",
+                    "24/7 priority support",
+                    "Advanced financial reporting",
+                    "Business growth analytics",
+                ]),
+            ],
+            [
+                'product' => 'gym-software',
+                'name' => 'Enterprise',
+                'price' => 99500.00,
+                'period' => 'One-Time',
+                'popular' => false,
+                'features' => json_encode([
+                    "Can manage 20,000+ members",
+                    "Can accept payments online/offline",
+                    "Member attendance can be tracked",
+                    "SMS system integrated",
+                    "Membership package tracking",
+                    "10+ users can use simultaneously",
+                    "24/7 dedicated support",
+                    "Complete business intelligence",
+                    "Customized reporting",
+                    "API access",
+                ]),
+            ],
         ]);
     }
 }
